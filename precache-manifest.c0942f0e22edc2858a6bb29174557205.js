319f32b9eacb7913d5163cbed008af105c55f98a
mihinduranasinghe <mihindu.21523@gmail.com>
2020-08-06 04:16:45 +0530

    Updates

---
+++ precache-manifest.c0942f0e22edc2858a6bb29174557205.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522d39ed35e02fca0a7e2029b63b6a63",
+    "url": "/elzian/index.html"
+  },
+  {
+    "revision": "4e9ad597904a809b8183",
+    "url": "/elzian/static/css/2.29b224f4.chunk.css"
+  },
+  {
+    "revision": "bbd353fc8974ae0a6d95",
+    "url": "/elzian/static/css/main.c2b0ea91.chunk.css"
+  },
+  {
+    "revision": "4e9ad597904a809b8183",
+    "url": "/elzian/static/js/2.0e328c10.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/elzian/static/js/2.0e328c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbd353fc8974ae0a6d95",
+    "url": "/elzian/static/js/main.b8bb1f37.chunk.js"
+  },
+  {
+    "revision": "957d1007833680708aa9",
+    "url": "/elzian/static/js/runtime-main.7f41751f.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/elzian/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/elzian/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/elzian/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/elzian/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/elzian/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
